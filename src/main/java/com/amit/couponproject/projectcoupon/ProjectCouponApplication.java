6f2239a80151586cfdb9385cf2df6ec80be00779
Amitop15 <amitop99874@gmail.com>
2022-04-03 19:35:40 +0300

    git init

---
+++ src/main/java/com/amit/couponproject/projectcoupon/ProjectCouponApplication.java
@@ -0,0 +1,13 @@
+package com.amit.couponproject.projectcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCouponApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amit/couponproject/projectcoupon/ProjectCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.couponproject.projectcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
